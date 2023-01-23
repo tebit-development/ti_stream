02a12f72e25952e2844b3bc091f933f61fca6472
unknown <bytesizedtrace@gmail.com>
2023-01-23 21:00:29 +1030

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'TebIT - Stream'
+version '1.0.0'
+
+files {
+    'metas/**/*.meta'
+}
+
+data_file 'CARCOLS_FILE'            'metas/**/*_carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'  'metas/**/*_carvariations.meta'
+data_file 'HANDLING_FILE'           'metas/**/*_handling.meta'
+data_file 'VEHICLE_METADATA_FILE'   'metas/**/*_vehicles.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    'metas/**/*_vehiclelayouts.meta'
